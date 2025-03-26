9b3d61d2d16d4c58dd19497ee0c0e4e36a011df6
ChasangBhutia <chasangtsering@gmail.com>
2025-03-27 00:49:46 +0530

    skills data added

---
+++ src/assets/SkillsData.jsx
@@ -0,0 +1,101 @@
+import html from "./html.png"
+import css from "./css.png"
+import javaScript from "./javascript.png"
+import react from "./react.png"
+import node from "./nodejs.png"
+import expressJs from "./expressjs.png"
+import bootstrap from "./bootstrap.png"
+import jquery from "./jquery.jpg"
+import mongoDb from "./mongodb.png"
+import mern from "./mern.png"
+import java from "./java.png"
+import python from "./python.png"
+import cpp from "./cpp.png"
+import kotlin from "./kotlin.png"
+
+
+import github from "./github.png"
+import vscode from "./vscode.png"
+import git from "./git.png"
+import postman from "./postman.png"
+import chatgpt from "./chatgpt.png"
+import figma from "./figma.png"
+
+export const TechnicalSkillsData =  [
+    {
+        imgUrl:html,
+        name:"HTML"
+
+    },{
+        imgUrl:css,
+        name:"CSS"
+
+    },{
+        imgUrl:javaScript,
+        name:"JavaScript"
+
+    },{
+        imgUrl:react,
+        name:"React.js"
+
+    },{
+        imgUrl:node,
+        name:"Node.js"
+
+    },{
+        imgUrl:expressJs,
+        name:"Express.js"
+
+    },{
+        imgUrl:bootstrap,
+        name:"Bootstrap"
+    },{
+        imgUrl:jquery,
+        name:"JQuery"
+    },{
+        imgUrl:mongoDb,
+        name:"MongoDB"
+    },{
+        imgUrl:mern,
+        name:"MERN"
+    },{
+        imgUrl:java,
+        name:"Java"
+    },{
+        imgUrl:python,
+        name:"Python"
+    },{
+        imgUrl:cpp,
+        name:"C++"
+    },{
+        imgUrl:kotlin,
+        name:"Kotlin"
+    }
+]
+
+export const ToolsData = [
+    {
+        imgUrl:github,
+        name:"Github"
+    },
+    {
+        imgUrl:git,
+        name:"Git"
+    },
+    {
+        imgUrl:vscode,
+        name:"VS Code"
+    },
+    {
+        imgUrl:postman,
+        name:"Postman"
+    },
+    {
+        imgUrl:chatgpt,
+        name:"ChatGPT"
+    },
+    {
+        imgUrl:figma,
+        name:"Figma"
+    }
+]

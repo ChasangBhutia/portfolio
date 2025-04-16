973c9366fc16cae7f46ee77a7eb0e45ae9fd5c8e
ChasangBhutia <chasangtsering@gmail.com>
2025-04-16 13:43:41 +0530

    update contact functionality

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Contact.css"
 import { motion } from 'framer-motion'
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
@@ -6,16 +6,40 @@ import EmailIcon from '@mui/icons-material/Email';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const Contact = () => {
+
+  const [formData, setFormData] = useState({
+    name:"",
+    email:"",
+    message:"",
+  });
+
+  function handleChange(e){
+    setFormData({...formData,[e.target.name] : e.target.value })
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const phoneNumber = "919339197277";
+    const {name,email,message} = formData;
+
+    const encodedMessage = encodeURIComponent(
+      `Hello, my name is ${name}.\nEmail: ${email}\nMessage: ${message}`
+    );
+
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(whatsappURL, "_blank");
+  }
+
   return (
     <div id='contact' className='section'>
       <h1 className='section-heading'>Contact</h1>
       <div className="contactContainer section-container flex-box">
       <motion.section initial={{x:"-30vw", opacity:0}} whileInView={{x:"0vw",opacity:1}} transition={{type:"spring", stiffness:50, duration:1}} viewport={{once:false, amount:0.4}} className="contactContainerLeft">
         <h2>Get in touch</h2>
-        <form action="mailto:chasangtsering@gmail.com" method="POST" encType="text/plain" className="flex-box">
-          <input type="text" name="name" placeholder="Your Name" required />
-          <input type="email" name="email" placeholder="Your Email" required />
-          <textarea name="message" placeholder="Write Message" rows="5" required></textarea>
+        <form onSubmit={handleSubmit} className="flex-box">
+          <input type="text" name="name" placeholder="Your Name" onChange={handleChange} value={formData.name} required />
+          <input type="email" name="email" placeholder="Your Email" onChange={handleChange} value={formData.email} required />
+          <textarea name="message" placeholder="Write Message" rows="5" onChange={handleChange} value={formData.message} required></textarea>
           <button type="submit">Submit</button>
         </form>
       </motion.section>
